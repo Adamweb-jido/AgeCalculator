3592aaa3139c0b78fda358d5228a3f4bceba159a
Adamweb-jido <realadamweb@gmail.com>
2025-07-08 11:43:21 +0100

    First changes

--- app/src/main/java/com/adamweb/agecalculator/MainActivity.kt
+++ app/src/main/java/com/adamweb/agecalculator/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.adamweb.agecalculator
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
     }
