ac0ed6979e03b13590ef6cc0cf2b4593ffb075de
Adamweb-jido <realadamweb@gmail.com>
2025-07-11 01:26:47 +0100

    Some changes

--- app/src/main/java/com/adamweb/agecalculator/MainActivity.kt
+++ app/src/main/java/com/adamweb/agecalculator/MainActivity.kt
@@ -2,12 +2,23 @@ package com.adamweb.agecalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.adamweb.agecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_AgeCalculator)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
+        val fragmentList = listOf(
+                firstOnboardFragment(),
+                SecondOnboardFragment(),
+                ThirdOnboardFragment()
+        )
+        val adapter = OnboardingAdapter(this, fragmentList)
+            binding.onboardViewPager.adapter = adapter
     }
 }
