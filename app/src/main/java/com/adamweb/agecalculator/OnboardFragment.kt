84ab46ce446bf08fefd08f23e8ce45d12fed8ca4
Adamweb-jido <realadamweb@gmail.com>
2025-07-12 13:29:47 +0100

    Weekend Changes

--- app/src/main/java/com/adamweb/agecalculator/OnboardFragment.kt
+++ app/src/main/java/com/adamweb/agecalculator/OnboardFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.adamweb.agecalculator.databinding.OnboardFragmentBinding
 
 class OnboardFragment : Fragment() {
@@ -27,7 +29,26 @@ class OnboardFragment : Fragment() {
         binding.onboardDesc.setText(args.getString("Descriptions"))
 
         val isLast = args.getBoolean("isLast", false)
-        binding.ageMeBtn.visibility = if (isLast) View.VISIBLE else View.GONE
+
+        binding.nextBtn.apply {
+            text = if (isLast) "Jump In" else "Next"
+            setOnClickListener {
+                if (isLast){
+                    Toast.makeText(context, "Welcome to Agetric", Toast.LENGTH_SHORT).show()
+                } else {
+                    val viewPager = activity?.findViewById<ViewPager2>(R.id.onboardViewPager)
+                    viewPager?.let {
+                        it.currentItem += 1
+                    }
+                }
+            }
+        }
+
+        binding.skipBtn.setOnClickListener {
+            Toast.makeText(context, "Welcome to Agetric", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 
 }
