4abc65ec33ce5d62a553bbe8b8f4527727b2437e
Adamweb-jido <realadamweb@gmail.com>
2025-07-13 14:46:20 +0100

    another changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.viewpager2)
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
---
+++ app/src/main/java/com/adamweb/agecalculator/SetUpProfileFragment.kt
@@ -0,0 +1,10 @@
+package com.adamweb.agecalculator
+
+import androidx.fragment.app.Fragment
+
+
+
+class SetUpProfileFragment : Fragment() {
+
+
+}
